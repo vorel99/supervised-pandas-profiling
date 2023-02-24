5f5595e792b5d4d525f17e13549d70c339cf1727
Jan Cap <vorel007@gmail.com>
2023-02-24 13:03:18 +0100

    create target description

---
+++ src/pandas_profiling/model/description_target.py
@@ -0,0 +1,74 @@
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Dict, List, Tuple
+
+from multimethod import multimethod
+from pandas_profiling.config import Settings
+
+
+@dataclass
+class TargetDescription(metaclass=ABCMeta):
+    """Description for target column."""
+
+    series: Any
+    series_binary: Any
+    config: Settings
+    name: str
+    description: Dict[str, Any]
+    positive_values: List[str]
+    negative_values: List[str]
+
+    def __init__(
+        self,
+        config: Settings,
+        series: Any,
+    ) -> None:
+        self.series = series
+        self.config = config
+        self.name = config.target.col_name
+        self.description = {}
+        self.positive_values, self.negative_values = self._infer_target_values()
+        self.series_binary = self._get_bin_target()
+        self._update_description_base()
+
+    @abstractmethod
+    def _infer_target_values(self) -> Tuple[List[str], List[str]]:
+        """Infer positive and negative values in target column.
+
+        Returns
+        -------
+        positive, negative : Tuple[List[str], List[str]]
+            Positive and negative values.
+        """
+        pass
+
+    @abstractmethod
+    def _get_bin_target(self) -> Any:
+        """Create binary target from column and positive/negative values.
+        Positive values replace with 1, negative with -1."""
+        pass
+
+    @abstractmethod
+    def _get_advanced_description(self) -> Dict[str, Any]:
+        """Update description for target variable.
+        Get target mean."""
+        pass
+
+    def _update_description_base(self) -> None:
+        """Update description.
+        Add positive and negative values."""
+        _desc = {}
+        _desc["positive_vals"] = self.positive_values
+        _desc["negative_vals"] = self.negative_values
+        _desc.update(self._get_advanced_description())
+
+        self.description.update(_desc)
+
+
+@multimethod
+def describe_target(
+    config: Settings,
+    data_frame: Any,
+) -> TargetDescription:
+    """Generate target description."""
+    raise NotImplementedError()
---
+++ src/pandas_profiling/model/pandas/describe_target_pandas.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+import numpy as np
+import pandas as pd
+from pandas_profiling.config import Settings
+from pandas_profiling.model.description_target import TargetDescription, describe_target
+
+
+class TargetDescriptionPandas(TargetDescription):
+    series: pd.Series
+    series_binary: pd.Series
+
+    def __init__(
+        self,
+        config: Settings,
+        series: pd.Series,
+    ) -> None:
+        series = series.astype(str)
+        super().__init__(config, series)
+
+    def _infer_target_values(self) -> Tuple[List[str], List[str]]:
+        """Infer positive and negative values."""
+        unique_vals = self.series.dropna().unique()
+        # user defined positive values
+        if self.config.target.positive_values is not None:
+            positive_vals = self.config.target.positive_values
+        # positive values are not defined
+        else:
+            positive_vals = []
+            for value in unique_vals:
+                if value in self.config.target.inferred_positive_values:
+                    positive_vals.append(value)
+
+        negative_vals = np.setdiff1d(unique_vals, positive_vals)
+        return positive_vals, list(negative_vals)
+
+    def _get_bin_target(self) -> pd.Series:
+        _bin_target = self.series.copy()
+        _bin_target.replace(self.positive_values, 1, inplace=True)
+        _bin_target.replace(self.negative_values, -1, inplace=True)
+        return _bin_target
+
+    def _get_advanced_description(self) -> Dict[str, Any]:
+        _desc = {}
+        _desc["target_mean"] = self.series_binary.mean()
+
+        return _desc
+
+
+@describe_target.register
+def describe_target_pandas(
+    config: Settings,
+    data_frame: pd.DataFrame,
+) -> TargetDescription:
+    if config.target.col_name is None:
+        raise ValueError("Target not defined.")
+    series = data_frame[config.target.col_name]
+    return TargetDescriptionPandas(config, series)
