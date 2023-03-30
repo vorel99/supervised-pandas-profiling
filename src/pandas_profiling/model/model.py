f9d15ecb495937ae8f804a73b7c9ceae8b0036e0
Jan Cap <vorel007@gmail.com>
2023-03-30 13:20:59 +0200

    update data preprocessor

--- src/pandas_profiling/model/model.py
+++ src/pandas_profiling/model/model.py
@@ -1,81 +1,45 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Any, Callable, Dict, Hashable
+from typing import Any, Dict, Hashable
+
+
+class Transformer(ABC):
+    """Abstract class for transformers."""
+
+    @abstractmethod
+    def fit(self, col: Any, col_desc: Dict[str, Any]):
+        """Fit col.
+
+        Args:
+            col (Any): Column with data to fit.
+            col_desc (Dict[str, Any]): Description of col.
+        """
+
+    @abstractmethod
+    def transform(self, col: Any, col_desc: Dict[str, Any]) -> Any:
+        """Transform col.
+
+        Args:
+            col (Any): Column with data to transform.
+            col_desc (Dict[str, Any]): Description of col.
+
+        Returns:
+            Any: Transformed col.
+        """
+
+    @abstractmethod
+    def fit_transform(self, col: Any, col_desc: Dict[str, Any]) -> Any:
+        """Fit and transform col.
+
+        Args:
+            col (Any): Column with data to fit and transform.
+            col_desc (Dict[str, Any]): Description of col.
+        Returns:
+            Any: Transformed col.
+        """
 
 
 class BaseDataProcessor(ABC):
-    col_map: Dict[str, Callable]
-    transformations: Dict[Hashable, Any] = {}
-
-    def __init__(self) -> None:
-        self.col_map = {
-            "Categorical": self.fit_cat,
-            "Numeric": self.fit_num,
-            "Text": self.fit_text,
-        }
-
-    @property
-    @abstractmethod
-    def data(self) -> Any:
-        """Preprocessed train data."""
-
-    @abstractmethod
-    def fit_num(self, col: Any, col_desc: Dict[str, Any]) -> Any:
-        """Transform numeric type column.
-        Save transformations to self.transformations.
-
-        Args:
-            col (Any): Data column.
-            col_desc (dict): Description of column.
-
-        Returns:
-            preprocessed (Any): Preprocessed numeric data.
-        """
-
-    @abstractmethod
-    def fit_cat(self, col: Any, col_desc: Dict[str, Any]) -> Any:
-        """Prepare categorical column.
-        Apply one-hot encoding.
-        Save transformations to self.transformations.
-
-        Args:
-            col (Any): Data column.
-            col_desc (dict): Description of column.
-
-        Returns:
-            preprocessed (Any): Preprocessed categorical data.
-        """
-
-    @abstractmethod
-    def fit_text(self, col: Any, col_desc: Dict[str, Any]) -> Any:
-        """Prepare text column.
-        Save transformations to self.transformations.
-
-        Args:
-            col (Any): Data column.
-            col_desc (dict): Description of column.
-
-        Returns:
-            preprocessed (Any): Preprocessed text data.
-        """
-
-    @abstractmethod
-    def _update_preprocessed_data(self, new_data: Any) -> None:
-        """Join preprocessed data with new data."""
-
-    def prepare_column(self, col: Any, col_desc: Dict[str, Any]):
-        """Prepare data column based on column type.
-        Add new data to preprocessed data.
-
-        Args:
-            col (Any): Data of column.
-            col_desc (Dict):  Description of column.
-        """
-        # call prepare function by column type
-        col_type = col_desc["type"]
-        prepare_func = self.col_map[col_type]
-        data = prepare_func(col, col_desc)
-        self._update_preprocessed_data(data)
+    transformations: Dict[Hashable, Transformer] = {}
 
     @abstractmethod
     def fit(self, data: Any, data_desc: Dict[str, Any]) -> None:
@@ -86,13 +50,5 @@ class BaseDataProcessor(ABC):
         """Train data processor on data and return preprocessed data."""
 
     @abstractmethod
-    def transform(self, test_data: Any):
+    def transform(self, test_data: Any) -> Any:
         """Transform test data same way as train data."""
-
-
-class BaseModel:
-    data_preprocessing: BaseDataProcessor
-
-    @property
-    def data(self) -> Any:
-        return self.data_preprocessing.data
--- src/pandas_profiling/model/pandas/model_pandas.py
+++ src/pandas_profiling/model/pandas/model_pandas.py
@@ -1,57 +1,110 @@
-from typing import Any, Dict
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Hashable, List
 
 import pandas as pd
-from pandas_profiling.model.model import BaseDataProcessor
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import xgboost as xgb
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+
+from pandas_profiling.model.model import BaseDataProcessor
+
+
+class TransformerPandas(ABC):
+    @abstractmethod
+    def fit(self, col: pd.Series, col_desc: Dict[str, Any]) -> None:
+        pass
+
+    @abstractmethod
+    def transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def fit_transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        pass
+
+
+class NumTransformerPandas(TransformerPandas):
+    """Transformer for numeric type variables."""
+
+    def fit(self, col: pd.Series, col_desc: Dict[str, Any]) -> None:
+        pass
+
+    def transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        return col.to_frame()
+
+    def fit_transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        self.fit(col, col_desc)
+        return self.transform(col, col_desc)
+
+
+class CatTransformerPandas(TransformerPandas):
+    """Transformer for category type variables.
+    - use one-hot encoding
+    """
+
+    transformer: OneHotEncoder
+
+    def fit(self, col: pd.Series, col_desc: Dict[str, Any]) -> None:
+        self.transformer = OneHotEncoder(handle_unknown="ignore")
+        self.transformer.fit(col.to_frame())
+
+    def transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        transformed = self.transformer.transform(col.to_frame()).toarray()
+        return pd.DataFrame(
+            transformed, columns=self.transformer.get_feature_names_out()
+        )
+
+    def fit_transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        self.fit(col, col_desc)
+        return self.transform(col, col_desc)
+
+
+class TextTransformerPandas(TransformerPandas):
+    """Transformer for category type variables.
+    - use tf-idf transformation
+    """
+
+    transformer: TfidfVectorizer
+    significant_words: List[str]
+
+    def _add_prefix(self, df: pd.DataFrame, name: str) -> pd.DataFrame:
+        return df.add_prefix(name + "_")
+
+    def fit(self, col: pd.Series, col_desc: Dict[str, Any]) -> None:
+        text_logodds: pd.DataFrame = col_desc["plot_description"].log_odds
+        # TODO replace constant .5
+        self.significant_words = text_logodds[
+            text_logodds["log_odds_ratio"].abs() > 0.5
+        ].index.to_list()
+
+        self.transformer = TfidfVectorizer(token_pattern=r"(?u)\b[\w\./]+\b")
+        # get tf-idf matrix of words
+        self.transformer.fit(col.values.astype(str))
+
+    def transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        transformed = self.transformer.transform(col.values.astype(str)).toarray()
+        data = pd.DataFrame(
+            transformed, columns=self.transformer.get_feature_names_out()
+        )
+        data = data[self.significant_words]
+        return self._add_prefix(data, col.name)
+
+    def fit_transform(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
+        self.fit(col, col_desc)
+        return self.transform(col, col_desc)
 
 
 class DataProcessorPandas(BaseDataProcessor):
-    preprocessed_data: pd.DataFrame
+    transform_map: Dict[str, TransformerPandas]
+    transformations: Dict[Hashable, TransformerPandas] = {}
 
     def __init__(self) -> None:
-        super().__init__()
-
-    @property
-    def data(self) -> Any:
-        return self.preprocessed_data
-
-    def fit_num(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
-        return col.to_frame()
-
-    def fit_cat(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
-        enc = OneHotEncoder(handle_unknown="ignore")
-        transformed = enc.fit_transform(col.to_frame()).toarray()
-        new_data = pd.DataFrame(transformed, columns=enc.get_feature_names_out())
-        self.transformations[col.name] = enc
-        return new_data
-
-    def fit_text(self, col: pd.Series, col_desc: Dict[str, Any]) -> pd.DataFrame:
-        text_logodds: pd.DataFrame = col_desc["plot_description"].log_odds
-        significant_words = text_logodds[
-            text_logodds["log_odds_ratio"].abs() > 0.5
-        ].index.to_list()
-        print(significant_words, col.name)
-        if len(significant_words) == 0:
-            return pd.DataFrame()
-        # get counts of significant words
-        pipe = Pipeline(
-            [
-                ("count", CountVectorizer(vocabulary=significant_words)),
-                ("tfidf", TfidfTransformer()),
-            ]
-        )
-        transformed = pipe.fit_transform(col).toarray()
-        new_data = pd.DataFrame(
-            transformed, columns=str(col.name) + "_" + pipe.get_feature_names_out()
-        )
-        self.transformations[col.name] = pipe
-        return new_data
-
-    def _update_preprocessed_data(self, new_data: pd.DataFrame) -> None:
-        self.preprocessed_data = self.preprocessed_data.join(new_data, how="outer")
+        self.transform_map = {
+            "Categorical": CatTransformerPandas,
+            "Numeric": NumTransformerPandas,
+            "Text": TextTransformerPandas,
+        }
 
     def fit(self, data: pd.DataFrame, data_desc: Dict[str, Any]) -> None:
         """Train data processor on data and data description.
@@ -60,7 +113,37 @@ class DataProcessorPandas(BaseDataProcessor):
             data (pd.DataFrame): Training data.
             data_desc (dict): Description of train data.
         """
-        self.fit_transform(data, data_desc)
+        for col_name, col_desc in data_desc.items():
+            if col_name not in data.columns:
+                continue
+            col = data[col_name]
+            col_type = col_desc["type"]
+            transformer = self.transform_map[col_type]()
+            transformer.fit(col, col_desc)
+            self.transformations[col_name] = transformer
+
+    def transform(self, data: pd.DataFrame, data_desc: Dict[str, Any]) -> pd.DataFrame:
+        """Transform test data.
+
+        Args:
+            test_data (pd.DataFrame): Test data, we want to transform.
+            data_desc (dict): Description of test data.
+
+        Returns:
+            pd.DataFrame: Transformed test data.
+        """
+
+        transformed_test = pd.DataFrame()
+        for col_name, col_desc in data_desc.items():
+            if col_name not in data.columns:
+                continue
+            test_col = data[col_name]
+            transformed_col = self.transformations[col_name].transform(
+                test_col, col_desc
+            )
+            transformed_test = transformed_test.join(transformed_col, how="outer")
+
+        return transformed_test
 
     def fit_transform(
         self, data: pd.DataFrame, data_desc: Dict[str, Any]
@@ -74,52 +157,5 @@ class DataProcessorPandas(BaseDataProcessor):
         Returns:
             pd.DataFrame: Preprocessed train data.
         """
-        self.preprocessed_data = pd.DataFrame()
-
-        for key, val in data_desc.items():
-            if key not in data.columns:
-                break
-            col = data[key]
-            self.prepare_column(col, val)
-        return self.data
-
-    def _transform_one_col(self, test_col: pd.Series) -> pd.DataFrame:
-        """Transform one column with transformer from fit transformer."""
-        transformed_col = test_col.to_frame()
-        if test_col.name in self.transformations.keys():
-            transformer = self.transformations[test_col.name]
-            transformed = transformer.transform(test_col.to_frame()).toarray()
-            transformed_col = pd.DataFrame(
-                transformed, columns=transformer.get_feature_names_out()
-            )
-        return transformed_col
-
-    def transform(
-        self, test_data: pd.DataFrame, data_desc: Dict[str, Any]
-    ) -> pd.DataFrame:
-        """Transform test data.
-
-        Args:
-            test_data (pd.DataFrame): Test data, we want to transform.
-            data_desc (dict): Description of test data.
-
-        Returns:
-            pd.DataFrame: Transformed test data.
-        """
-        if self.preprocessed_data is None:
-            raise ValueError("You need t fit data first.")
-
-        transformed_test = pd.DataFrame()
-        for key, val in data_desc.items():
-            if key not in test_data.columns:
-                raise ValueError("'{}' is in description, but not in data.".format(key))
-            test_col = test_data[key]
-            transformed_col = self._transform_one_col(test_col)
-            transformed_test = transformed_test.join(transformed_col, how="outer")
-
-        return transformed_test
-
-
-class ModelPandas:
-    def __init__(self) -> None:
-        pass
+        self.fit(data, data_desc)
+        return self.transform(data, data_desc)
