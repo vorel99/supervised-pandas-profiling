2b2c8e29d51c8fb3e2ee8260c7a373e907a546b9
Jan Cap <vorel007@gmail.com>
2023-05-10 15:51:58 +0200

    replace round with fmt_numeric at categorycal render

--- src/pandas_profiling/report/structure/variables/render_categorical.py
+++ src/pandas_profiling/report/structure/variables/render_categorical.py
@@ -394,7 +394,7 @@ def render_categorical(config: Settings, summary: dict) -> dict:
         rows.append(
             {
                 "name": "p-value of independence",
-                "value": round(
+                "value": fmt_numeric(
                     p_value_of_independence,
                     config.report.precision,
                 ),
